6e9c0f89ef5790665b1638d91565fd81adc12f66
Edgar Reyes <82401247+Comfyish@users.noreply.github.com>
2021-05-06 14:09:06 -0700

    Initial Program Upload

---
+++ DOTFC/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject health;
+    public GameObject player;
+    public bool pause = false;
+    public GameObject paused;
+    public bool GameEnd = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DOTFC/Assets/Scripts/playerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D myRB;
+    public Vector2 velocity;
+    public Vector2 respawnPos;
+    private Quaternion zero;
+    public GameManager gm;
+    public int health = 5;
+    public float speed = 4;
+
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        respawnPos = new Vector2(0, 0);
+        zero = new Quaternion();
+
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = myRB.velocity;
+        velocity.x = Input.GetAxisRaw("Horizontal") * speed;
+        myRB.velocity = velocity;
+
+    }
+}
