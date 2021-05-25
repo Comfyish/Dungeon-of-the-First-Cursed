0c753a2646238ea70fd36e29d6c640e9b3dc440f
Comfyish <82401247+Comfyish@users.noreply.github.com>
2021-05-25 16:27:08 -0700

    Working Main Menu

---
+++ DOTFC/Assets/MainMenuManager.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void loadLevel1()
+    {
+        StartCoroutine("levelLoader", "Level1");
+
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator levelLoader(string levelName)
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(levelName);
+    }
+
+
+}
